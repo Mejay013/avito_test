acf0ed67a33ec34089afa0b8a6f6ef84dc8ca53f
Mejay <arseniy.kocharov012@gmail.com>
2020-09-26 23:02:05 +0300

    add validation test

--- shortening_link/views.py
+++ shortening_link/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
 import pyshorteners
-
+from django.core.validators import URLValidator
 
 
 def index(request):
-    if request.method == "POST":
-        old_link = request.POST['old_link']
-        if old_link:
-
-            s = pyshorteners.Shortener()
-            new_link = s.tinyurl.short(old_link)
-            context = {
-                'old_link': old_link,
-                'new_link' : new_link
-            }
-            return render(request, 'shortening_link/index.html',context)
+    try: 
+        if request.method == "POST":
+            val = URLValidator()
+            warning_list = list()
+            old_link = request.POST['old_link']
+            test = val(old_link)
+            if old_link and test != 'invalid':
+                s = pyshorteners.Shortener()
+                new_link = s.tinyurl.short(old_link)
+                context = {
+                    'old_link': old_link,
+                    'new_link' : new_link
+                }
+                return render(request, 'shortening_link/index.html',context)
+    except:
+        warning_list.append('Проверьте корректность введенной ссылки')
+        return render(request, 'shortening_link/index.html',{'warning_list': warning_list})
     return render(request, 'shortening_link/index.html')
