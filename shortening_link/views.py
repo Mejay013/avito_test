af0dd5cc38d6b578eac9e347c7ea5e973f648b1e
Mejay <arseniy.kocharov012@gmail.com>
2020-09-26 22:16:44 +0300

    add shorted func

--- shortening_link/views.py
+++ shortening_link/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
+import pyshorteners
+
 
 
 def index(request):
     if request.method == "POST":
         old_link = request.POST['old_link']
         if old_link:
+
+            s = pyshorteners.Shortener()
+            new_link = s.tinyurl.short(old_link)
             context = {
-                'old_link': old_link
+                'old_link': old_link,
+                'new_link' : new_link
             }
             return render(request, 'shortening_link/index.html',context)
     return render(request, 'shortening_link/index.html')
