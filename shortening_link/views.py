1c60349332992679e0bbcfd820adf297c3e22bb3
Mejay <arseniy.kocharov012@gmail.com>
2020-09-27 02:57:40 +0300

    debug

--- shortening_link/views.py
+++ shortening_link/views.py
@@ -6,20 +6,19 @@ from django.core.validators import URLValidator
 
 def index(request):
     try: 
-        if request.method == "POST":
-            val = URLValidator()
-            warning_list = list()
-            old_link = request.POST['old_link']
-            test = val(old_link)
-            if old_link and test != 'invalid':
-                s = pyshorteners.Shortener()
-                new_link = s.tinyurl.short(old_link)
+        if request.method == "POST": 
+            val = URLValidator() 
+            warning_list = list() # список для ошибок
+            old_link = request.POST['old_link'] 
+            test = val(old_link) # проверяем валидность ссылки
+            if old_link and test != 'invalid': # Если ссылки валидна и не пустая
+                s = pyshorteners.Shortener()  
+                new_link = s.tinyurl.short(old_link) # используем pyshorteners для создания короткой ссылки
                 context = {
-                    'old_link': old_link,
                     'new_link' : new_link
                 }
-                return render(request, 'shortening_link/index.html',context)
+                return render(request, 'shortening_link/index.html',context) # отправляем ссылку шаблону
     except:
         warning_list.append('Проверьте корректность введенной ссылки')
-        return render(request, 'shortening_link/index.html',{'warning_list': warning_list})
+        return render(request, 'shortening_link/index.html',{'warning_list': warning_list}) # отправляем список ошибок шаблону
     return render(request, 'shortening_link/index.html')
