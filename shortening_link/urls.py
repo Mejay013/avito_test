8bfcf979db143b5e0b7154b601064efdc29129d2
Mejay <arseniy.kocharov012@gmail.com>
2020-09-26 20:06:46 +0300

    add index and base template

--- shortening_link/urls.py
+++ shortening_link/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name="shortening_link"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/index', views.index, name='index')
 ]
--- shortening_link/views.py
+++ shortening_link/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpRequest
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == "POST":
+        old_link = request.POST['old_link']
+        if old_link:
+            context = {
+                'old_link': old_link
+            }
+            return render(request, 'shortening_link/index.html',context)
+    return render(request, 'shortening_link/index.html')
